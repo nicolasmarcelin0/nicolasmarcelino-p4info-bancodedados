42881c3d9f2e0de26cb33724def53fbe29c15ee1
nicolasmarcelino <89655379+nicolasmarcelino@users.noreply.github.com>
2022-04-26 09:25:52 -0300

    Create questao-03.sql

---
+++ atividades/atividade-avaliação/avaliacao-03/questao-03.sql
@@ -0,0 +1,36 @@
+INSERT INTO TB_PRODUTO (
+	COD_PROD,
+	NOME PROD, 
+	PRECO_UNIT
+)
+VALUES
+    (
+      100,
+      'Arroz Tio João',
+      6.00
+    ),
+    (
+      150,
+      'Feijão Carioquinha',
+      5.50
+    ),
+    (
+      200,
+      'Macarrão Fortaleza',
+      3.50
+    ),
+    (
+      250,
+      'Oleo de Soja',
+      4.00
+    ),
+    (
+      300,
+      'Manteiga Betania 500g',
+      8.00
+    ),
+    (
+      350,
+      'Queijo Ricota Betania',
+      7.00
+    );
