fe888e24fd21c6447bdb6a7af71f6a6728e4f63b
nicolasmarcelino <89655379+nicolasmarcelino@users.noreply.github.com>
2022-04-26 09:18:19 -0300

    Create questao-01.sql

---
+++ atividades/atividade-avaliação/avaliacao-03/questao-01.sql
@@ -0,0 +1,27 @@
+INSERT INTO TB_CLIENTES (
+	COD_CLI,
+	NOME_CLI,
+	END_CLI
+)
+VALUES
+	(
+		001,
+		'José Maria Alves',
+		'Av João Pessoa 2081¹
+	),
+	(
+		002,
+		'Maria Conceição Tavares',
+		'Rua Waldery Uchoa 4¹
+	),	
+	(
+		003,
+		'João Cosme Fonseca',
+		'Rua Padre Franscisco Pinto 790¹
+	),	
+	(
+		004,
+		'Tiago Cosme Fonseca',
+		'Rua Padre Franscisco Pinto 790'
+	);
+
