cdb2185f6619a222aed341ffd4dace7d46e06351
nicolasmarcelino <89655379+nicolasmarcelino@users.noreply.github.com>
2022-04-26 09:18:54 -0300

    Update questao-01.sql

--- atividades/atividade-avaliação/avaliacao-03/questao-01.sql
+++ atividades/atividade-avaliação/avaliacao-03/questao-01.sql
@@ -7,17 +7,17 @@ VALUES
 	(
 		001,
 		'José Maria Alves',
-		'Av João Pessoa 2081¹
+		'Av João Pessoa 2081'
 	),
 	(
 		002,
 		'Maria Conceição Tavares',
-		'Rua Waldery Uchoa 4¹
+		'Rua Waldery Uchoa 4'
 	),	
 	(
 		003,
 		'João Cosme Fonseca',
-		'Rua Padre Franscisco Pinto 790¹
+		'Rua Padre Franscisco Pinto 790'
 	),	
 	(
 		004,
