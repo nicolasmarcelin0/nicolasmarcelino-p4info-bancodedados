8d49fc6c619958d92f440868b0f126647e6769f4
nicolasmarcelino <89655379+nicolasmarcelino@users.noreply.github.com>
2022-04-26 09:21:44 -0300

    Create questao-02.sql

---
+++ atividades/atividade-avaliação/avaliacao-03/questao-02.sql
@@ -0,0 +1,21 @@
+INSERT INTO TB VENDEDORES (
+	COD_VEND,
+	NOME_VEND
+)
+VALUES
+	(
+		1,
+		'Luciano Arruda Cavalcante'
+	),
+	(
+		2,
+		'Joana Alves Pessoa'
+	),
+	(
+		3,
+		'Mercia Bessa Santos'
+	),
+	(
+		4,
+		'Antonio de Padua Lopes'
+	);
